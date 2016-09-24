844f010dcd06bf125183c6311d38c61b9f418738
codyjs <c_s7@txstate.edu>
2016-09-24 01:15:54 -0500

    add middleware injection example

---
+++ MiddlewareInjection/main.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata';
+import { Kernel, injectable } from 'inversify';
+import * as express from 'express';
+import * as morgan from 'morgan';
+import { interfaces, InversifyExpressServer, TYPE, Get, Controller } from 'inversify-express-utils';
+import { controllerFactory } from './controller';
+
+let kernel = new Kernel();
+
+kernel.bind<express.RequestHandler>('Morgan').toConstantValue(morgan('combined'));
+kernel.bind<express.RequestHandler>('CustomMiddleware').toConstantValue(function customMiddleware(req: any, res: any, next: any) {
+    req.user = {
+        username: 'foo',
+        password: 'bar'
+    };
+    next();
+});
+
+let controller = controllerFactory(kernel);
+kernel.bind<interfaces.Controller>(TYPE.Controller).to(controller).whenTargetNamed('TestController');
+
+let server = new InversifyExpressServer(kernel);
+server.build().listen('3000', () => {console.log('Listening on port 3000')});
