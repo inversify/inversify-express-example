516375379bdeeac2663166ef5b150e82c7b4f0bf
Steven Siahetiong <ssiahetiong@gmail.com>
2017-06-25 23:29:03 +0800

    Errors on bindingdecorators example (#209)
    * Fix bindingdecorators example
    
    * Remove duplicate dependency

--- BindingDecorators/ioc/loader.ts
+++ BindingDecorators/ioc/loader.ts
@@ -1,5 +1,5 @@
 // when a module is loaded @provide() will automatically register it
-import './controller/home';
-import './utils/mongodb/client';
-import './controller/user';
-import './service/user';
+import '../controller/home';
+import '../utils/mongodb/client';
+import '../controller/user';
+import '../service/user';
