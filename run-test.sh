dc4628d0203032293ec46b9c8a3b34825db7c2a3
remojansen <remo.jansen@wolksoftware.com>
2017-11-07 01:46:32 +0000

    trying to fix build

--- run-test.sh
+++ run-test.sh
@@ -1,1 +1,1 @@
-nyc --require ts-node/register --require reflect-metadata/Reflect --extension .ts -- mocha --timeout 5000 **/*.spec.ts
+nyc --clean --all --require ts-node/register --require reflect-metadata/Reflect --extension .ts -- mocha --timeout 5000 **/*.spec.ts
